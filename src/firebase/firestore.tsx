f0120fa07819c773ed0e6a9c65782da34a9c47d8
othienoivan <othienoivan@gmail.com>
2026-07-03 20:07:38 +0300

    Add student registration with Firebase

---
+++ src/firebase/firestore.tsx
@@ -0,0 +1,29 @@
+import { doc, serverTimestamp, setDoc } from "firebase/firestore";
+import { db } from "../config/firebase";
+
+type CreateUserProfileData = {
+  uid: string;
+  fullName: string;
+  email: string;
+  role?: "student" | "tutor" | "admin";
+};
+
+export async function createUserProfile({
+  uid,
+  fullName,
+  email,
+  role = "student",
+}: CreateUserProfileData) {
+  const userRef = doc(db, "users", uid);
+
+  await setDoc(userRef, {
+    uid,
+    fullName,
+    email,
+    role,
+    profilePhoto: "",
+    enrolledCourses: [],
+    createdAt: serverTimestamp(),
+    updatedAt: serverTimestamp(),
+  });
+}
